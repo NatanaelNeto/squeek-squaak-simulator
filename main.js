2ee4ebffb4a8fd0fea1816bac4b3fb6121cd53cc
Natanael Neto <natanael.eneas2018@hotmail.com>
2019-05-21 18:29:36 -0300

    Atualização

--- main.js
+++ main.js
@@ -49,31 +49,58 @@ function playGame(array){
    var x = 0;
    var concluido = false;
    var init,end;
+   var texto = document.getElementById("startSpan");
+   texto.style.backgroundColor = "#C71606";
+   texto.innerHTML = "COMEÇOU!";
    init = new Date();
    document.addEventListener('keyup',(event) =>{
       if(!concluido){
          if(x === array.length-1){
             if((event.keyCode === 65 && array[x] === 0)||(event.keyCode === 76 && array[x] === 1)){
                console.log("concluido");
+               texto.innerHTML = "CONCLUÍDO!";
+               texto.style.backgroundColor = "rgba(0,0,0,0)";
                concluido = true;
                end = new Date();
-               console.log(Math.floor((end-init)/array.length));
+               calculateMedia(init,end,array.length);
             }else{
                x=0;
+               texto.innerHTML = "ERRADO!";
                console.log(x);
             }
          }else if(event.keyCode === 65 && array[x] === 0){
             x++;
+            texto.innerHTML = "CERTO";
             console.log(x);
          }else if(event.keyCode === 76 && array[x] === 1){
             x++;
+            texto.innerHTML = "CERTO";
             console.log(x);
          }else{
             x=0;
+            texto.innerHTML = "ERRADO!";
             console.log(x);
          }
       }else{
          return
       }
    });
+}
+
+function calculateMedia(a,b,c){
+   var media = Math.floor((b-a)/c);
+   var result = document.getElementById("result");
+   if(media < 400){
+      result.style.color = "#C71606";
+      result.innerHTML = "Muito rápido! Seu resultado foi: " + media + "ms para 500ms";
+   }else if(media >= 400 && media < 480){
+      result.style.color = "#c7c706";
+      result.innerHTML = "Bom, mas pode melhorar indo mais devagar. Seu resultado foi: " + media + "ms para 500ms";
+   }else if(media >= 480 && media < 520){
+      result.style.color = "#06c706";
+      result.innerHTML = "Muito bom! Você acertou o ritmo! Seu resultado foi: " + media + "ms para 500ms";
+   }else if(media >= 520 && media < 600){
+      result.style.color = "#C71606";
+      result.innerHTML = "Muito lento! Seu resultado foi: " + media + "ms para 500ms";
+   }
 }
